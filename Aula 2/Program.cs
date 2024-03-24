d42ffb163f799311c9af1fa88fc08016721ef660
Jessyka Ázara <142222515+jessykadev@users.noreply.github.com>
2024-03-24 11:55:42 -0300

    first commit

---
+++ Aula 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Testando";
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
+//ponto de pausa
